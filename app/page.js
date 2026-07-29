66e697c975dee8b88dc170bfa066b205b90b3063
Christine Walker <your@email.com>
2026-07-29 14:09:05 +0100

    Fix import

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import dynamic from 'next/dynamic'
-const HHCApp = dynamic(() => import('./HHCApp.jsx').then(m => m.default || m), { ssr: false })
+const HHCApp = dynamic(() => import('./HHCApp.jsx').then(m => ({ default: m.App || m.default })), { ssr: false })
 export default function Page() {
   return <HHCApp />
 }
